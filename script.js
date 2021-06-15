50e134cfc0fc628eece0278675606ad53bf4f7d7
Oduwole Samuel <gahlaktus30000@gmail.com>
2021-06-15 22:55:40 +0200

    updates

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-const root = document.documentElement;
-const postForm = document.getElementById("post-form");
-const midSection = document.getElementById("section-middle");
-
-const nav = document.querySelector(".nav");
-const boxSection = document.querySelector(".status-box");
-const popUp = document.querySelector(".status-popup");
-
-const secs = document.querySelectorAll(".sec");
+const root = document.documentElement,
+  postForm = document.getElementById("post-form"),
+  midSection = document.getElementById("section-middle"),
+  nav = document.querySelector(".nav"),
+  secs = document.querySelectorAll(".sec");
 
 const btnMore = `
     <div class="status-popup hidden">
@@ -15,56 +11,102 @@ const btnMore = `
       <span class="delete"><a href="#">delete</a></span>
       <span class="privacy"><a href="#">privacy</a></span>
       <span class="report"><a href="#">report</a></span>
+    </div>`,
+  deleteBoxTemplate = `
+    <div class="delete-box ">
+      <h3>DELETE</h3>
+      <p class="delete-content">you are about to delete this post, do you want to continue?</p>
+      <p class="delete-dialog">
+        <span><a href="#">delete</a></span>
+        <span><a href="#">cancel</a></span>
+      </p>
+    </div>`,
+  statusTemplate = `
+    <div class="status-box">
+      <div class="status-info">
+        <div class="user-image-box">
+          <img src="./img/IMG_sam_3234353.jpg" alt="" class="user-image" />
+        </div>
+        <p>
+          <span class="username">oduwole samuel</span>
+          <span class="date">22 min.</span>
+        </p>
+        <span>⭐</span>
+      </div>
+      <p class="status-content">{{post}}</p>
+      <p class="status-content-dialog">
+        <span class="status-like">
+          <a href="#">likes</a>
+          <span> 99+</span>
+        </span>
+        <span class="status-comment">
+          <a href="#">comments</a>
+          <span> 9</span>
+        </span>
+        <span class="status-more">
+          <a href="#">more</a>
+        </span>
+      </p>
     </div>`;
 
-
-const deleteBox=`<div class="delete-box">
-<h3>DELETE</h3>
-<p class="delete-content">you are about to delete this post, do you want to continue?</p>
-<p class="delete-dialog">
-  <span><a href="#">delete</a></span>
-  <span><a href="#">cancel</a></span>
-</p>
-</div>`
-
-function cb(e) {
+function submitForm(e) {
   e.preventDefault();
-  const [[, post]] = Array.from(new FormData(this));
-  // const { post } = Object.fromEntries(new FormData(this));
+
+  // const [[, post]] = Array.from(new FormData(e.target));
+  const { post } = Object.fromEntries(new FormData(e.target));
   if (!post) return;
 
-  const status = `        <div class="status-box">
-  <div class="status-info">
-    <img src="./img/IMG_sam_3234353.jpg" alt="" class="user-image" />
-    <p>
-      <span class="username">oduwole samuel</span
-      ><span class="date">22 min.</span>
-    </p>
-    <span>⭐</span>
-  </div>
-  <p class="status-content">
-   ${post}
-  </p>
-  <p class="status-content-dialog">
-    <span class="status-like"
-      ><a href="#">likes</a><span> 99+</span></span
-    >
-    <span class="status-comment"
-      ><a href="#">comments</a><span> 9</span></span
-    >
-    <span class="status-more"><a href="#">more</a></span>
-  </p>
-</div>`;
+  const status = statusTemplate.replace("{{post}}", post);
 
-  // const p = document.createElement("p");
-  // p.textContent = post;
+  e.target.insertAdjacentHTML("afterend", status);
 
-  this.insertAdjacentHTML("afterend", status);
-  Array.from(this.children).forEach((obj) =>
-    ["post"].includes(obj.name) ? (obj.value = "") : undefined
+  Array.from(e.target.children).forEach((domEl) =>
+    ["post"].includes(domEl.name) ? (domEl.value = "") : undefined
   );
 
   // status?.removeEventListener("submit", cb);
+}
+
+let temp;
+function clickHandler(e) {
+  const popUp = document.querySelector(".status-popup");
+  const deleteBox = document.querySelector(".delete-box");
+
+  const popUpEvent = e.target.closest(".status-popup");
+  const more = e.target.closest(".status-more");
+
+  const statusBox = e.target.closest(".status-box");
+  let share, edit, deleteOption, privacy, report;
+
+  if (!popUp && !more) return;
+
+  e.preventDefault();
+  popUp && setTimeout(() => popUp?.remove(), 50);
+
+  if (more) {
+    deleteBox?.insertAdjacentElement("beforebegin", temp);
+    deleteBox?.remove();
+    temp = undefined;
+
+    setTimeout(() => more?.insertAdjacentHTML("beforebegin", btnMore), 50);
+    // more?.insertAdjacentHTML("beforebegin", btnMore);
+  }
+
+  if (popUp)
+    [share, edit, deleteOption, privacy, report] = Array.from(
+      popUp.children
+    ).map((obj) => obj.classList.value);
+
+  if (e.target.closest("." + deleteOption)) {
+    statusBox.insertAdjacentHTML("afterend", deleteBoxTemplate);
+    statusBox.remove();
+
+    setTimeout(() => {
+      document.querySelector(".delete-box").style.transform = "scale(1)";
+    }, 80);
+
+    temp = statusBox;
+  }
 }
 
 // function navCb(entries, observer) {
@@ -98,44 +140,9 @@ function secCb(entries, observer) {
 const secObserver = new IntersectionObserver(secCb);
 secs.forEach((s) => secObserver.observe(s));
 
-postForm?.addEventListener("submit", cb);
-
-root?.addEventListener("click", function (e) {
-  if (
-    !document.querySelector(".status-popup") &&
-    !e.target.closest(".status-more")
-  )
-    return;
-
-  if (
-    document.querySelector(".status-popup") &&
-    !["share", "edit", "delete", "privacy", "report"].some((cl) =>
-      e.target.closest("." + cl)
-    )
-  )
-    document.querySelector(".status-popup").remove();
-
-  e.preventDefault();
-
-  if (e.target.closest(".status-more"))
-    e.target.closest(".status-more").insertAdjacentHTML("beforebegin", btnMore);  
-  
-
-  if (
-    ["share", "edit", "privacy", "report"].some((cl) =>
-      e.target.closest("." + cl)
-    )
-  ) {
-    console.log(e.target.closest("span"));
-    document.querySelector(".status-popup").remove();
-
-  }
-
-  if(e.target.closest(".delete")){
-    e.target.closest(".status-box").style.display="none" 
-    e.target.closest(".status-box")?.insertAdjacentHTML("afterend",deleteBox)
-    document.querySelector(".status-popup").remove();
-  }
-
-
-});
+["submit", "click"].forEach((type, i, arr) =>
+  root.addEventListener(type, function (e) {
+    e.type === arr[0] && submitForm(e);
+    e.type === arr[1] && clickHandler(e);
+  })
+);
